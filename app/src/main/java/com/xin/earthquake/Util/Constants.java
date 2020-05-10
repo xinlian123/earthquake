1321bdac8228e4ac656f0847d4777261748eeeba
lianx <lianxin9342@gmail.com>
2020-05-10 17:27:09 -0400

    first version

---
+++ app/src/main/java/com/xin/earthquake/Util/Constants.java
@@ -0,0 +1,6 @@
+package com.xin.earthquake.Util;
+
+public class Constants {
+    public static final String URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+    public static final int LIMIT = 1000;
+}
